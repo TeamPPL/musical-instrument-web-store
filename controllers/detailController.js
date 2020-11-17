91cdb9ab366775639614ebadcc70210fba73598f
Lundi <49914842+minhluannguyen@users.noreply.github.com>
2020-11-17 12:04:26 +0700

    Conver back to GET

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -1,8 +1,10 @@
 const productModel = require('../models/productModel');
 
 exports.index = (req, res, next) => {
-    const idProduct = parseInt(req.body.ID);
+    const idProduct = parseInt(req.query.ID);
 
     const productItems = productModel.list()[idProduct - 1];
+    console.log(productItems);
+    console.log(idProduct);
     res.render('detail/detail', {productItems , idProduct});
 };
