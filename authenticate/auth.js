968a4ecc4d5b3db68c7a7f01f57f31ca62f6110e
Lundi <49914842+minhluannguyen@users.noreply.github.com>
2020-11-29 16:55:55 +0700

    Initial login and signup
    Finish compare account between client and server, add new account to DB

---
+++ authenticate/auth.js
@@ -0,0 +1,51 @@
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+const accountModel = require('../models/accountModel');
+
+module.exports = (app) => {
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser((user, done) => {
+        console.log(user);
+        done(null, user._id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
+        let account = await accountModel.findById(id);
+
+        if (account == null) 
+        {
+          done(null, false);
+        }
+        else
+        {
+          done(null, account);
+        }
+     });
+    
+    passport.use(
+      new LocalStrategy( async (username, password, done) => {
+        let account = await accountModel.findByUsername(username);
+        console.log(account);
+        if (account == null) 
+        {
+          return done(null, false);
+        }
+
+        
+
+        if (!bcrypt.compare(password, account.password)) 
+        //if (password !== account.password)
+        {
+          return done(null, false);
+        }
+        else 
+        {
+          return done(null, account);
+        }
+      })
+    );
+};
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,10 +1,40 @@
 
+const bcrypt = require('bcrypt');
 const accountModel = require('../models/accountModel');
 
-exports.login = (req, res, next) => {
+exports.getLogin = (req, res, next) => {
     res.render('user/login');
 };
 
-exports.signup = (req, res, next) => {
+exports.getSignup = (req, res, next) => {
     res.render('user/signup');
 };
+
+exports.createNewAccount = async (req, res, next) => {
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    const phone = req.body.phone;
+    const email = req.body.email;
+
+    let account = await accountModel.findByUsername(username);
+
+    if (account !== null) 
+    {
+        //Ton tai username roi
+    } else {
+        //Chua co username nay
+        let hash = bcrypt.hashSync(password, 10);
+        let accountInfos = {
+            "username": username,
+            "password": hash,
+            "name": name,
+            "phone": phone,
+            "email": email
+        };
+
+        accountModel.insertOne(accountInfos);
+    }
+
+    next();
+}
