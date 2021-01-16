0c34d1f648a32a5255c00e8c7c6dbfaea85943cd
Lundi <49914842+minhluannguyen@users.noreply.github.com>
2021-01-16 07:32:37 +0700

    Add links for carousel

--- models/carouselModel.js
+++ models/carouselModel.js
@@ -4,31 +4,36 @@ exports.list = () => {
             id: 1,
             title: 'Guitars',
             cover: 'https://previews.123rf.com/images/sheftsoff/sheftsoff1708/sheftsoff170805808/84081834-beautiful-girl-play-music-with-guitar-young-woman-portrait-with-acoustic-guitar-.jpg',
-            description: "Classic, electric, acoustic and more..."
+            description: "Classic, electric, acoustic and more...",
+            filter: 'guitar',
         },
         {
             id: 2,
             title: 'Violins',
             cover: 'https://i.pinimg.com/564x/23/bc/dd/23bcdd3869b2674ce728c759f0feb36d.jpg',
-            description: "Violin, viola, cello, double bass,..."
+            description: "Violin, viola, cello, double bass,...",
+            filter: 'violin',
         },
         {
             id: 3,
             title: 'Keyboards',
             cover: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cab2dc58-138e-462c-9071-c83cf7e66389/d1323qr-32b0dcd3-0183-468e-bb34-f6432b74cde3.jpg/v1/fill/w_600,h_600,q_75,strp/piano_by_jmillar13_d1323qr-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD02MDAiLCJwYXRoIjoiXC9mXC9jYWIyZGM1OC0xMzhlLTQ2MmMtOTA3MS1jODNjZjdlNjYzODlcL2QxMzIzcXItMzJiMGRjZDMtMDE4My00NjhlLWJiMzQtZjY0MzJiNzRjZGUzLmpwZyIsIndpZHRoIjoiPD02MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.jjO5Csz3x-PoBOUkx3xvVsx_B5gqLmQf_R7UoMU9AXI',
-            description: "Grand, light, ..."
+            description: "Grand, light, ...",
+            filter: 'piano',
         },
         {
             id: 4,
             title: 'Drums',
             cover: 'https://i.pinimg.com/564x/36/ee/74/36ee74efcf99c0a9a9aa7846e7e520e0.jpg',
-            description: "Drums go boom! pang! pew pew!!!"
+            description: "Drums go boom! pang! pew pew!!!",
+            filter: 'drum',
         },
         {
             id: 5,
             title: 'Other',
             cover: 'https://i.pinimg.com/originals/5c/2f/bf/5c2fbf7bfffdb479492be879f54bcd83.png',
-            description: "<3 <3 <3"
+            description: "<3 <3 <3",
+            filter: 'all',
         },
     ]
 }
